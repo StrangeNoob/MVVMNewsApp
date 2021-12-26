95c8870fc5c9e7c379aa1232bb514421aeff3c00
StrangeNoob <sipun63427452@gmail.com>
2021-12-27 01:18:33 +0530

    Intial Commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Constants {
+    companion object{
+        const val API_KEY = "5ecd3cb4a4c54ea88193ce35a7415efc"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
